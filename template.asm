781b76d2dd52fc100fba04ce7037b4f2c6d1c405
UziMonkey <uzimonkey@gmail.com>
2019-12-01 01:11:55 -0500

    Initial commit

---
+++ template.asm
@@ -0,0 +1,38 @@
+;vim: ts=8,sw=8
+.include "common.inc"
+
+.union Params
+.endunion
+
+.macro VARS _
+;  name			bytes	default value
+.endmacro
+
+.macro SPRITES _
+;  name			y		sprite	flags		x
+.endmacro
+
+.segment	"STARTUP"
+
+.segment	"ZEROPAGE"
+VARS ZPAGE_DEFINITIONS
+params:		.tag Params
+
+.segment	"CODE"
+zpagedefaults:
+VARS ZPAGE_DEFAULTS
+zpagedefaultslen = * - zpagedefaults
+
+spritedata:	
+SPRITES SPRITEDATA
+spritedatalen = * - spritedata
+
+
+
+.segment	"CODE"
+start:		lda #123
+		jsr putint
+
+		rts
+
+update:		rts
